f7e11632ccca34f1435326b41ba44ad4ef4540d9
fausc22 <faausc@gmail.com>
2025-12-31 12:49:23 -0300

    widget2

--- components/HeroVideo.tsx
+++ components/HeroVideo.tsx
@@ -56,7 +56,6 @@ export default function HeroVideo() {
             muted
             playsInline
             poster="/hero-poster.jpg"
-            loading="lazy"
             className="absolute top-1/2 left-1/2 w-full h-full object-cover"
             style={{
               transform: "translate(-50%, -50%)",
